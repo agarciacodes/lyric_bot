9bd6820e1d85c55d80afb8777fe1098d20d5d3bc
agarciacodes <70783190+agarciacodes@users.noreply.github.com>
2022-10-10 12:03:03 -0400

    Update main.py

--- main.py
+++ main.py
@@ -4,21 +4,20 @@ import tweepy
 
 # set up twitter access keys
 keys = {
-	'CONSUMER_API_KEY': 'TWjkgrfmTMnjDQiT5HIyw405X',
-	'CONSUMER_API_SECRET_KEY': 'Cn9nFqySicXl86fG88wA93GyiRrzLyB7q8qgV3Ge3nA1tJiclh',
-	'ACCESS_TOKEN': '1312520754542632964-0NqA1PamRm4IhAImoL4QVtAClfZwCx',
-	'ACCESS_TOKEN_SECRET': 'qwYqxKNueu9enXEpXbgRxwUbapQoxGVTyjwCbDxce6jgE'}
+	'CONSUMER_API_KEY': 'COSUMER_API_KEY_HERE',
+	'CONSUMER_API_SECRET_KEY': 'CONSUMER_API_SECRET_KEY_HERE',
+	'ACCESS_TOKEN': 'ACCESS_TOKEN_HERE',
+	'ACCESS_TOKEN_SECRET': 'ACCESS_TOKEN_SECRET_HERE'}
 
-all_songs = ['Wat’s Wrong', '4r da Squaw', 'Heavenly Father', 'Shot You Down (Remix)', "Free Lunch", "Park", "Silkk da Shocka", "West Savannah", "Tity and Dolla", "Shot U Down", "R.I.P. Kevin Miller", "Ronnie Drake", "Smile", "Modest", "Stuck in the Mud", "Menthol", "AA", "Headshots (4r Da Locals)", "Rope // rosegold", "Soliloquy", "Webbie Flow (U Like)", "Cilvia Demo", "Banana", "Tranquility", "Dressed Like Rappers", "RIP Young", "Brad Jordan", "Bday", "Lay Wit Ya", "Find a Topic (homies begged)", "From the Garden", "Hereditary", "HB2U", "Nelly", "Brenda", "Hii (Fuck Love)", "Wat U Sed", "Claymore", "Part III", "A lot", "Darkseid", "where u at?", "Score", "THIB", "Chad", "Hurt Cobaine", "by george (outro)", "Hey Mista", "True Story", "2x Pills", "Don’t Matter", "GIL / Sounds from Friday Morning", "Mani Lux", "Sydney Jones", "Why Worry", "Weak Shit", "Food for Thought", "Fake Trill", "All Herb", "Don’t Shoot", "Grace", "200/Warning", "Like That", "Gusto", "9-3 Freestyle", "i mean", "Part II", "Khaki", "95", "RUNNIN’ 4RM DA LAW", "The Spill (Grammy Family Freestyle)", "The Race Freestyle", "Donuts", "RIP Young (Remix)", "XXL Freshman Freestyle", "Deep Blue", "Geordan Favors", "Part IV", "Runnin’", "Genius Freestyle 002", "Sounds from Friday Morning", "Slow Motion (Freestyle)", "Worm", "Calculator", "Fingernails*", "How I Started", "Elevators", "Fears", "Whales", "Punch Drunk (Love Love)", "Ain’t Trippin’", "Heavenly Father x TDE", "See", "Sway in the Morning Freestyle", "Seasons (Demo)", "Bat Cave*", "Monkey Bites BACK", "On My Mind*", "Gatorade", "Loose Change", "Vibers (Prayers)", "Demo Outro", "PEANUT", "Ashley", "Unknown", "Toca Tuesdays Freestyle", "GIL", "Junkies Interlude", "Mista Ride Tha Whip*", "Ask About Me", "S.B.B.B. (Goblin)", "Weak Shit 2*", "Wings Spread*", "PSA", "Group Freestyles", "Voyage to Atlanta", "Music (Where is He)", "Blaze*", "Voltron", "C.R.E.A.M.", "Dirty Sanchez", "Hoarder", "Villain", "Kabook*", "Chattanooga", "Mista", "Pulse", "Saturday*", "Up In Smoke”, 'Geek*', '2 Pills (Hands Out)', 'B32', 'Quicksand', 'RapFix Freestyle', 'From The Garden (Demo)', 'GAS', 'The Basement", 'From the Garden (OG)", "Vomit',
-"Frozens", "Strange Love", "Ery’day Situations", "From The Garden (Original)", "Snippet", "Donuts (OG)", "Wat U Sed (OG)", "Break A Habit", "Score (OG)"]
+all_songs = [list_of_your_artist_song_here]
 
 # get a random song's lyrics
 
 def get_raw_lyrics():
-	genius_client_access_token = "_k7abKQsKkPRRxQi2JAdt3tGThZg7TeB9skk3F1DK1rE1bh436cBtBdWvyORS0EF"
-	genius = lyricsgenius.Genius('_k7abKQsKkPRRxQi2JAdt3tGThZg7TeB9skk3F1DK1rE1bh436cBtBdWvyORS0EF')
+	genius_client_access_token = "genius_client_access_token_here"
+	genius = lyricsgenius.Genius('genius_client_access_token_here')
 	random_song_title = random.choice(all_songs)
-	lyrics = genius.search_song(random_song_title, "Isaiah Rashad").lyrics
+	lyrics = genius.search_song(random_song_title, "Artist_name").lyrics
 	song = random_song_title.upper()
 	return lyrics, song
 
